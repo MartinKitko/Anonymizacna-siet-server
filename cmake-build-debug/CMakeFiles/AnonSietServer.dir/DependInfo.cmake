b7567b0055a13f2c83e4d45058fadd3735b67ec5
MartinKitko <martin.kitko736@gmail.com>
2022-12-16 11:23:33 +0100

    prvy commit

---
+++ cmake-build-debug/CMakeFiles/AnonSietServer.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/tmp.GRlTpNue1y/definitions.c" "/tmp/tmp.GRlTpNue1y/cmake-build-debug/CMakeFiles/AnonSietServer.dir/definitions.c.o"
+  "/tmp/tmp.GRlTpNue1y/main.c" "/tmp/tmp.GRlTpNue1y/cmake-build-debug/CMakeFiles/AnonSietServer.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/AnonSietServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AnonSietServer"
+  "AnonSietServer.pdb"
+  "CMakeFiles/AnonSietServer.dir/definitions.c.o"
+  "CMakeFiles/AnonSietServer.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AnonSietServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
